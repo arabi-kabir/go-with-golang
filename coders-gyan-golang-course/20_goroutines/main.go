eb0411614369e9b86adef61c6993ce2b40f6d09b
Arabi Kabir <mail2arabi@gmail.com>
2026-07-26 00:10:44 +0600

    20 goroutine done

---
+++ coders-gyan-golang-course/20_goroutines/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func task(id int, w *sync.WaitGroup) {
+	defer w.Done()
+	fmt.Println("doing task ", id)
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	for i := 0; i <= 10; i++ {
+		wg.Add(1)
+		time.Sleep(1 * time.Second)
+		go task(i, &wg)
+	}
+
+	wg.Wait()
+
+}
